89444ae869408038979d95ebf079b96dea149ca1
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-01-06 01:51:59 +0530

    find duplicate in an array of N+1 Integers

--- Array/findDuplicate.cpp
+++ Array/findDuplicate.cpp
@@ -1,3 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+        for(int i = 0 ; i < nums.size() ; i++)
+        {
+            int index = abs(nums[i]);
+
+            if(nums[index] < 0) return index;
+
+            nums[index] = -nums[index];
+        }
+
+        return -1;
+    }
+};
+//floyd-algorithm
 int tortoise = nums[0];
         int hare = nums[0];
 
