ab1ab55ee2ef17dddc96cfd7f9345a6dd8e0e544
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-01-06 00:16:16 +0530

    Update maxSubarraySum.cpp

--- Array/maxSubarraySum.cpp
+++ Array/maxSubarraySum.cpp
@@ -12,3 +12,23 @@ long long maxSubarraySum(int arr[], int n){
         
         return res;
     }
+
+long long maxSubarraySum(int arr[], int n)
+{
+    /*
+        Don't write main().
+        Don't read input, it is passed as function argument.    
+        No need to print anything.
+        Taking input and printing output is handled automatically.
+    */
+    long long currSum = 0;
+    long long mx = INT_MIN;
+    for( int i = 0; i < n; i++){
+        currSum += arr[i];
+        if( currSum < 0){
+            currSum = 0;
+        }
+        mx = max( currSum , mx );
+    }
+    return mx;
+}
