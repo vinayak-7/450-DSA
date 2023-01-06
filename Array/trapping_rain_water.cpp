70af12a843a43bcd44ee5a2e29a468ed767bd7ad
Vinayak Bandhu <84093533+vinayak-7@users.noreply.github.com>
2023-01-07 01:15:11 +0530

    Create trapping_rain_water.cpp

---
+++ Array/trapping_rain_water.cpp
@@ -0,0 +1,26 @@
+long long trappingWater(int arr[], int n){
+        // code here
+        long long int res = 0;
+        int lmax[n] , rmax[n];
+        lmax[0] = arr[0];
+        
+        for(int i = 1; i < n ; i++)
+        {
+            lmax[i] = max(arr[i],lmax[i-1]);
+        }
+
+        rmax[n-1] = arr[n-1];
+        
+        for(int i = n-2 ; i >= 0 ; i--)
+        {
+            rmax[i] = max(arr[i],rmax[i+1]);
+        }
+        
+        for(int i = 1; i < n ; i++)
+        {
+            res = res + (min(lmax[i] , rmax[i]) - arr[i]);
+        }
+        
+        return res;
+        
+    }
